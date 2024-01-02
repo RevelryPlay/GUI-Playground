d6c5c43773ba7e574c1aadca12a40f11876bf505
Kylie Benfield <kylie@benfield.design>
2024-01-01 17:13:22 -0700

    Adding Imgui OpenGL rendering

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include "ConsoleMenu.hpp"
 
+#include "imguiRenderer.cpp"
+
 int main()
 {
     MainMenu main("main", "Main Menu");
 
-    Menu openGlSub ("opengl", "OpenGL Menu");
-    Menu vulkanSub ("vulkan", "Vulkan Menu");
+    Menu openGlSub("opengl", "OpenGL Menu");
+    Menu vulkanSub("vulkan", "Vulkan Menu");
 
     main.AddAction({"qt", "QT", []() { std::cout << "QT" << std::endl; }});
-    main.AddAction({"imgui", "IMGUI", []() { std::cout << "IMGUI" << std::endl; }});
     main.AddAction({"gltk", "GLTK", []() { std::cout << "GLTK" << std::endl; }});
     main.AddAction({"wxwidgets", "WXWIDGETS", []() { std::cout << "WXWIDGETS" << std::endl; }});
     main.AddAction({"slint", "SLINT", []() { std::cout << "SLINT" << std::endl; }});
+
+    openGlSub.AddAction({"imgui", "IMGUI", []()
+    {
+        ImguiRenderer::renderOpenGL();
+    }});
 
     main.AddMenu(openGlSub);
     main.AddMenu(vulkanSub);
